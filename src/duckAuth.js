d2f1f55a5ea1899a9f62bec999626c01c6c91746
Constantine Gippius <konstantin.gippius@t-systems.com>
2021-04-01 17:20:42 +0300

    refactor: fetch in duckAuth

--- src/duckAuth.js
+++ src/duckAuth.js
@@ -12,12 +12,6 @@ export const register = (username, password, email) => {
   .then((response) => {
     return response.json();
   })
-  // 1. убрать лишний then
-  .then((res) => {
-    return res;
-  })
-  // 1. убрать catch - ошибки должны обрабатываться там, где вызывается функция
-  .catch((err) => console.log(err));
 };
 export const authorize = (identifier, password) => {
   return fetch(`${BASE_URL}/auth/local`, {
@@ -29,17 +23,6 @@ export const authorize = (identifier, password) => {
     body: JSON.stringify({identifier, password})
   })
   .then((response => response.json()))
-  // 1. убрать обработку снизу в родительский компонент
-  .then((data) => {
-    if (data.user){
-      localStorage.setItem('jwt', data.jwt);
-      return data;
-    } else {
-      return;
-    }
-  })
-  // 1. убрать catch - ошибки должны обрабатываться там, где вызывается функция
-  .catch(err => console.log(err))
 };
 export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
@@ -50,10 +33,9 @@ export const getContent = (token) => {
       'Authorization': `Bearer ${token}`,
     }
   })
-  // 1. два then снизу можно заменить на 
-  // const checkResponse = (res) => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.statusText}`)}
-  .then(res => res.json())
-  .then(data => data)
+  .then(checkResponse)
 }
+
+const checkResponse = (res) => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.statusText}`)
 
 
